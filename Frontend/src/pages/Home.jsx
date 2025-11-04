14f36d13974bb82d7e2e77dbe925ad100bf8a672
Somanatha Basavanuty <somanatha.clg@gmail.com>
2025-11-04 20:10:06 +0530

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	renamed:    Backend/public/assets/index-BhM3ZRpI.js -> Backend/public/assets/index-BN1q2XlZ.js #	modified:   Backend/public/index.html #	modified:   Frontend/src/pages/Home.jsx #	modified:   Frontend/src/pages/Login.jsx #	modified:   Frontend/src/pages/Register.jsx #

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
     if (title) title = title.trim();
     if (!title) return
 
-    const response = await axios.post("https://cohort-1-project-chat-gpt.onrender.com/api/chat", {
+    const response = await axios.post("https://cohort-1-project-chat-gpt-lvp3.onrender.com/api/chat", {
       title
     }, {
       withCredentials: true
@@ -61,12 +61,12 @@ const Home = () => {
   // Ensure at least one chat exists initially
   useEffect(() => {
 
-    axios.get("https://cohort-1-project-chat-gpt.onrender.com/api/chat", { withCredentials: true })
+    axios.get("https://cohort-1-project-chat-gpt-lvp3.onrender.com/api/chat", { withCredentials: true })
       .then(response => {
         dispatch(setChats(response.data.chats.reverse()));
       })
 
-    const tempSocket = io("https://cohort-1-project-chat-gpt.onrender.com", {
+    const tempSocket = io("https://cohort-1-project-chat-gpt-lvp3.onrender.com", {
       withCredentials: true,
     })
 
@@ -119,7 +119,7 @@ const Home = () => {
 
   const getMessages = async (chatId) => {
 
-   const response = await  axios.get(`https://cohort-1-project-chat-gpt.onrender.com/api/chat/messages/${chatId}`, { withCredentials: true })
+   const response = await  axios.get(`https://cohort-1-project-chat-gpt-lvp3.onrender.com/api/chat/messages/${chatId}`, { withCredentials: true })
 
    console.log("Fetched messages:", response.data.messages);
 
